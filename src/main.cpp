86fa404186ae82c0c3f2a465300b212ee5728f56
kuroppe <kuroppems@gmail.com>
2020-07-12 19:04:35 +0900

    The LCD display is changed if the button clicked

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,15 @@
 #include <SPI.h>
 #include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
 
+#define TP_PIN_PIN 33
+#define TP_PWR_PIN 25
+#define LED_PIN 4
+
 TFT_eSPI tft = TFT_eSPI(); // Invoke library, pins defined in User_Setup.h
+
+uint8_t func_select = 0;
+boolean pressed = false;
+uint8_t MAX_FUNC_SELECT = 1;
 
 void setup()
 {
@@ -11,11 +19,39 @@ void setup()
     tft.init();
     tft.setRotation(1);
     tft.setSwapBytes(true);
+
+    pinMode(TP_PIN_PIN, INPUT);
+    pinMode(TP_PWR_PIN, PULLUP); //! Must be set to pull-up output mode in order to wake up in deep sleep mode
+    digitalWrite(TP_PWR_PIN, HIGH);
 }
 
 void loop()
 {
-    tft.setTextColor(TFT_SKYBLUE, TFT_BLACK);
-    tft.setTextDatum(MC_DATUM);
-    tft.drawString("Hello world", tft.width() / 2, tft.height() / 2, 4);
+    if (digitalRead(TP_PIN_PIN) == HIGH) {
+        if (!pressed) {
+            tft.fillScreen(TFT_BLACK);
+            func_select = func_select + 1 > MAX_FUNC_SELECT ? 0 : func_select + 1;
+            digitalWrite(LED_PIN, HIGH);
+            delay(100);
+            digitalWrite(LED_PIN, LOW);
+            pressed = true;
+        }
+    } else {
+        pressed = false;
+    }
+
+    switch (func_select) {
+    case 0:
+        tft.setTextColor(TFT_SKYBLUE, TFT_BLACK);
+        tft.setTextDatum(MC_DATUM);
+        tft.drawString("HOGE", tft.width() / 2, tft.height() / 2, 4);
+        break;
+    case 1:
+        tft.setTextColor(TFT_SKYBLUE, TFT_BLACK);
+        tft.setTextDatum(MC_DATUM);
+        tft.drawString("PIYO", tft.width() / 2, tft.height() / 2, 2);
+        break;
+    default:
+        break;
+    }
 }
