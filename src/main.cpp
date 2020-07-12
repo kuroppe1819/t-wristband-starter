ee2225267ec7ad6b885cee9e19799f881b542682
kuroppe <kuroppems@gmail.com>
2020-07-12 17:05:56 +0900

    Show "Hello world" on LCD display

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
 
-void setup() {
-  // put your setup code here, to run once:
+TFT_eSPI tft = TFT_eSPI(); // Invoke library, pins defined in User_Setup.h
+
+void setup()
+{
+    Serial.begin(115200);
+
+    tft.init();
+    tft.setRotation(1);
+    tft.setSwapBytes(true);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    tft.setTextColor(TFT_SKYBLUE, TFT_BLACK);
+    tft.setTextDatum(MC_DATUM);
+    tft.drawString("Hello world", tft.width() / 2, tft.height() / 2, 4);
 }
